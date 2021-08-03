a52c5bc51999e84a3f18f4f90cf02f121c8cfe9b
Chris Parton <chris@southernmobile.com.au>
2021-08-04 08:53:25 +1000

    Add controller with basic SQL query

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Controller("/hello")
+public class HelloController {
+
+    private DataSource dataSource;
+
+    public HelloController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Get(produces = MediaType.TEXT_PLAIN)
+    public String test() throws SQLException {
+        ResultSet rs = dataSource.getConnection().createStatement().executeQuery("SELECT 1");
+        rs.next();
+        return "Result: " + rs.getInt(1);
+    }
+}
