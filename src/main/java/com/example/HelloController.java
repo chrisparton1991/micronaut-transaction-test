fbed34779b3bb336cc75b5d78da3738f97f2df05
Chris Parton <chris@southernmobile.com.au>
2021-08-04 10:32:00 +1000

    Get @Transactional onto the classpath (tranasctions still not working)

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,7 +3,7 @@ package com.example;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-
+import org.springframework.transaction.annotation.Transactional;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,13 +11,13 @@ import java.sql.SQLException;
 @Controller("/hello")
 public class HelloController {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public HelloController(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    // @Transactional TODO annotation not on classpath.
+    @Transactional
     @Get(produces = MediaType.TEXT_PLAIN)
     public String test() throws SQLException {
         ResultSet rs = dataSource.getConnection().createStatement().executeQuery("SELECT 1");
