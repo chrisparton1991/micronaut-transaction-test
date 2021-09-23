a568aa4914830e50060e8d293dfab09434d3b31a
Chris Parton <chris@southernmobile.com.au>
2021-09-23 14:22:24 +1000

    Attempt to use @Transactional

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,25 +3,26 @@ package com.example;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@Controller("/hello")
+@Controller("/test")
 public class HelloController {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public HelloController(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    // @Transactional TODO annotation not on classpath.
+    @Transactional
     @Get(produces = MediaType.TEXT_PLAIN)
     public String test() throws SQLException {
-        ResultSet rs = dataSource.getConnection().createStatement().executeQuery("SELECT 1");
-        rs.next();
-        return "Result: " + rs.getInt(1);
+        // Expected: This method should be called inside a transaction, so we see a "Roll back" error message each time.
+        // Actual: This method isn't inside a transaction, so the first call creates the table, and subsequent calls fail because the table exists.
+        dataSource.getConnection().createStatement().executeUpdate("CREATE TABLE TEST(id INT)");
+        throw new RuntimeException("Roll back.");
     }
 }
